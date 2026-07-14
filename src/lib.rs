91d4067002b25f65cffea229a916fa4856197c8c
misaka10987 <misaka10987@outlook.com>
2026-07-14 09:29:10 +0800

    init commit

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,231 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use creeper_maven_coord::MavenCoord;
+use mc_launchermeta::{VersionKind, version::Arguments};
+use reqwest::Client;
+use serde::{
+    Deserialize, Serialize,
+    de::{DeserializeOwned, IgnoredAny},
+};
+use serde_with::{DisplayFromStr, serde_as};
+use url::Url;
+
+pub const META_API: &str = "https://meta.fabricmc.net/";
+
+pub struct FabricMetaClient {
+    http: Client,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl FabricMetaClient {
+    pub fn new(http: Client) -> Self {
+        Self { http }
     }
+
+    async fn get_meta<T: DeserializeOwned>(&self, path: &str) -> anyhow::Result<T> {
+        let path = path.strip_prefix("/").unwrap_or(path);
+
+        let url = META_API.parse::<Url>().unwrap().join(path)?;
+
+        let res = self.http.get(url).send().await?.json().await?;
+
+        Ok(res)
+    }
+
+    pub async fn versions(&self) -> anyhow::Result<FabricMeta> {
+        self.get_meta("/v2/versions").await
+    }
+
+    pub async fn game_versions(&self) -> anyhow::Result<Vec<Game>> {
+        self.get_meta("/v2/versions/game").await
+    }
+
+    pub async fn game_versions_yarn(&self) -> anyhow::Result<Vec<Game>> {
+        self.get_meta("/v2/versions/game/yarn").await
+    }
+
+    pub async fn game_versions_intermediary(&self) -> anyhow::Result<Vec<Game>> {
+        self.get_meta("/v2/versions/game/intermediary").await
+    }
+
+    pub async fn intermediary_versions(&self) -> anyhow::Result<Vec<Intermediary>> {
+        self.get_meta("/v2/versions/intermediary").await
+    }
+
+    pub async fn game_intermediary_versions(
+        &self,
+        game: &str,
+    ) -> anyhow::Result<Vec<Intermediary>> {
+        let path = format!("/v2/versions/intermediary/{game}");
+
+        self.get_meta(&path).await
+    }
+
+    pub async fn yarn_versions(&self) -> anyhow::Result<Vec<Mapping>> {
+        self.get_meta("/v2/versions/yarn").await
+    }
+
+    pub async fn game_yarn_versions(&self, game: &str) -> anyhow::Result<Vec<Mapping>> {
+        let path = format!("/v2/versions/yarn/{game}");
+
+        self.get_meta(&path).await
+    }
+
+    pub async fn loader_versions(&self) -> anyhow::Result<Vec<Loader>> {
+        self.get_meta("/v2/versions/loader").await
+    }
+
+    pub async fn game_loader_versions(
+        &self,
+        game: &str,
+    ) -> anyhow::Result<Vec<LoaderWithIntermediary>> {
+        let path = format!("/v2/versions/loader/{game}");
+
+        self.get_meta(&path).await
+    }
+
+    pub async fn profile(&self, game: &str, loader: &str) -> anyhow::Result<Profile> {
+        let path = format!("/v2/versions/loader/{game}/{loader}/profile/json");
+
+        self.get_meta(&path).await
+    }
+}
+
+#[derive(Clone, Serialize, Deserialize)]
+#[serde(deny_unknown_fields, rename_all = "camelCase")]
+pub struct FabricMeta {
+    #[serde(default, skip_serializing_if = "Vec::is_empty")]
+    pub game: Vec<Game>,
+
+    #[serde(default, skip_serializing_if = "Vec::is_empty")]
+    pub mappings: Vec<Mapping>,
+
+    #[serde(default, skip_serializing_if = "Vec::is_empty")]
+    pub intermediary: Vec<Intermediary>,
+
+    #[serde(default, skip_serializing_if = "Vec::is_empty")]
+    pub loader: Vec<Loader>,
+
+    #[serde(default, skip_serializing_if = "Vec::is_empty")]
+    pub installer: Vec<Installer>,
+}
+
+#[derive(Clone, Serialize, Deserialize)]
+#[serde(deny_unknown_fields, rename_all = "camelCase")]
+pub struct Game {
+    /// The version of the game.
+    ///
+    /// Minecraft's version number may not be a valid semver.
+    pub version: String,
+
+    pub stable: bool,
+}
+
+#[serde_as]
+#[derive(Clone, Serialize, Deserialize)]
+#[serde(deny_unknown_fields, rename_all = "camelCase")]
+pub struct Mapping {
+    pub game_version: String,
+
+    pub separator: String,
+
+    pub build: u64,
+
+    #[serde_as(as = "DisplayFromStr")]
+    pub maven: MavenCoord,
+
+    pub version: String,
+
+    pub stable: bool,
+}
+
+#[serde_as]
+#[derive(Clone, Serialize, Deserialize)]
+#[serde(deny_unknown_fields, rename_all = "camelCase")]
+pub struct Intermediary {
+    #[serde_as(as = "DisplayFromStr")]
+    pub maven: MavenCoord,
+
+    pub version: String,
+
+    pub stable: bool,
+}
+
+#[serde_as]
+#[derive(Clone, Serialize, Deserialize)]
+#[serde(deny_unknown_fields, rename_all = "camelCase")]
+pub struct Loader {
+    pub separator: String,
+
+    pub build: u64,
+
+    #[serde_as(as = "DisplayFromStr")]
+    pub maven: MavenCoord,
+
+    pub version: String,
+
+    pub stable: bool,
+}
+
+#[serde_as]
+#[derive(Clone, Serialize, Deserialize)]
+#[serde(deny_unknown_fields, rename_all = "camelCase")]
+pub struct Installer {
+    pub url: Url,
+
+    #[serde_as(as = "DisplayFromStr")]
+    pub maven: MavenCoord,
+
+    pub version: String,
+
+    pub stable: bool,
+}
+
+#[derive(Clone, Serialize, Deserialize)]
+#[serde(deny_unknown_fields, rename_all = "camelCase")]
+pub struct LoaderWithIntermediary {
+    pub loader: Loader,
+
+    pub intermediary: Intermediary,
+
+    #[serde(rename = "launcherMeta", skip_serializing)]
+    pub _launcher_meta: Option<IgnoredAny>,
+}
+
+#[derive(Clone, Serialize, Deserialize)]
+#[serde(deny_unknown_fields, rename_all = "camelCase")]
+pub struct Profile {
+    pub id: String,
+
+    pub inherits_from: String,
+
+    pub release_time: String,
+
+    pub time: String,
+
+    #[serde(rename = "type")]
+    pub kind: VersionKind,
+
+    pub main_class: String,
+
+    pub arguments: Arguments,
+
+    pub libraries: Vec<Library>,
+}
+
+#[serde_as]
+#[derive(Clone, Serialize, Deserialize)]
+#[serde(deny_unknown_fields, rename_all = "camelCase")]
+pub struct Library {
+    #[serde_as(as = "DisplayFromStr")]
+    pub name: MavenCoord,
+
+    pub url: Url,
+
+    pub md5: Option<String>,
+
+    pub sha1: Option<String>,
+
+    pub sha256: Option<String>,
+
+    pub sha512: Option<String>,
+
+    pub size: Option<u64>,
 }
